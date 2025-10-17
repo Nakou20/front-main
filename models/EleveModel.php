dda04016352317bf9d2356b9e69f699f67368db7
sasha mangin2006 <sashamangin2006@gmail.com>
2025-10-17 16:53:59 +0200

    retirer le print de mise a jour d info

--- models/EleveModel.php
+++ models/EleveModel.php
@@ -196,8 +196,6 @@ class EleveModel extends SQL
         }
 
         // Exécuter la requête
-        print_r($motDePasse);
-        print_r($query);
         $stmt = $pdo->prepare($query);
 
         $result = $stmt->execute($params);
